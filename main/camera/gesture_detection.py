696f2488ca62bf7524d57997ab38c20f79a2adc1
Ben Dodd <ben-dodd@outlook.com>
2023-02-13 23:45:01 +0000

    Thread-ify finger counting

--- main/camera/gesture_detection.py
+++ main/camera/gesture_detection.py
@@ -3,70 +3,68 @@ Gesture detection using MediaPipe
 Author: Benjamin Dodd (1901386)
 """
 
+import threading
+
 import cv2 as cv
 import mediapipe as mp
+
+from . import LOGGER
+
+from .gesture_repository import GESTURE_REPOSITORY
+from .video_feed import VideoFrame
 
 
 MP_DRAWING = mp.solutions.drawing_utils
 MP_DRAWING_STYLES = mp.solutions.drawing_styles
 MP_HANDS = mp.solutions.hands
 
-VIDEO_CAPTURE = cv.VideoCapture(0)
-VIDEO_CAPTURE.set(cv.CAP_PROP_FPS, 60)
+class GestureDetection(threading.Thread):
+    """Thread for detecting gestures in a frame
+    """
+    def __init__(self, frame: VideoFrame):
+        super().__init__()
+        self.__image = frame.to_pillow()
 
+    def run(self):
+        with MP_HANDS.Hands(
+                model_complexity=0,
+                min_detection_confidence=0.5,
+                min_tracking_confidence=0.5) as hands:
+            self.__image.flags.writeable = False
+            results = hands.process(self.__image)
+            self.__image.flags.writeable = True
 
-with MP_HANDS.Hands(
-        model_complexity=0,
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as hands:
-    while VIDEO_CAPTURE.isOpened():
-        success, image = VIDEO_CAPTURE.read()
-        if not success:
-            # Skip processing as the frame was not captured
-            continue
-
-        # Flip the image horizontally for a later selfie-view display, and convert
-        # the BGR image to RGB.
-        image = cv.cvtColor(cv.flip(image, 1), cv.COLOR_BGR2RGB)
-        # To improve performance, optionally mark the image as not writeable to
-        # pass by reference.
-        image.flags.writeable = False
-        results = hands.process(image)
-
-        # Draw the hand annotations on the image.
-        image.flags.writeable = True
-        image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
-        if results.multi_hand_landmarks:
             finger_count = 0
-            last_index_x, last_index_y = 0, 0
-            for hand_landmarks in results.multi_hand_landmarks:
-                # print(
-                #     'Index finger tip coordinates: (',
-                #     f'{hand_landmarks.landmark[MP_HANDS.HandLandmark.INDEX_FINGER_TIP].x * image.shape[0]}, '
-                #     f'{hand_landmarks.landmark[MP_HANDS.HandLandmark.INDEX_FINGER_TIP].y * image.shape[1]})'
-                # )
-                last_index_x = hand_landmarks.landmark[MP_HANDS.HandLandmark.INDEX_FINGER_TIP].x * image.shape[0]
-                last_index_y = hand_landmarks.landmark[MP_HANDS.HandLandmark.INDEX_FINGER_TIP].y * image.shape[1]
-                if hand_landmarks.landmark[MP_HANDS.HandLandmark.INDEX_FINGER_TIP].y < hand_landmarks.landmark[MP_HANDS.HandLandmark.INDEX_FINGER_DIP].y:
-                    finger_count += 1
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    if hand_landmarks.landmark[MP_HANDS.HandLandmark.INDEX_FINGER_TIP].y < hand_landmarks.landmark[MP_HANDS.HandLandmark.INDEX_FINGER_DIP].y:
+                        finger_count += 1
 
-                if hand_landmarks.landmark[MP_HANDS.HandLandmark.MIDDLE_FINGER_TIP].y < hand_landmarks.landmark[MP_HANDS.HandLandmark.MIDDLE_FINGER_DIP].y:
-                    finger_count += 1
+                    if hand_landmarks.landmark[MP_HANDS.HandLandmark.MIDDLE_FINGER_TIP].y < hand_landmarks.landmark[MP_HANDS.HandLandmark.MIDDLE_FINGER_DIP].y:
+                        finger_count += 1
 
-                if hand_landmarks.landmark[MP_HANDS.HandLandmark.RING_FINGER_TIP].y < hand_landmarks.landmark[MP_HANDS.HandLandmark.RING_FINGER_DIP].y:
-                    finger_count += 1
+                    if hand_landmarks.landmark[MP_HANDS.HandLandmark.RING_FINGER_TIP].y < hand_landmarks.landmark[MP_HANDS.HandLandmark.RING_FINGER_DIP].y:
+                        finger_count += 1
 
-                if hand_landmarks.landmark[MP_HANDS.HandLandmark.PINKY_TIP].y < hand_landmarks.landmark[MP_HANDS.HandLandmark.PINKY_DIP].y:
-                    finger_count += 1
+                    if hand_landmarks.landmark[MP_HANDS.HandLandmark.PINKY_TIP].y < hand_landmarks.landmark[MP_HANDS.HandLandmark.PINKY_DIP].y:
+                        finger_count += 1
 
-                MP_DRAWING.draw_landmarks(
-                    image=image,
-                    landmark_list=hand_landmarks,
-                    connections=MP_HANDS.HAND_CONNECTIONS,
-                    landmark_drawing_spec=MP_DRAWING_STYLES.get_default_hand_landmarks_style(),
-                    connection_drawing_spec=MP_DRAWING_STYLES.get_default_hand_connections_style())
-            cv.putText(image, str(f"You are holding up {finger_count} digits"), (int(last_index_x), int(last_index_y)-20), cv.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+            GESTURE_REPOSITORY.current_gesture = finger_count
 
-        cv.imshow('Hands', image)
+if __name__ == '__main__':
+    from .video_feed import VIDEO_FEED
+
+    while True:
+        video_frame = VIDEO_FEED.capture()
+        if video_frame:
+            gesture_detection = GestureDetection(video_frame)
+            gesture_detection.start()
+            gesture_detection.join()
+            # draw the gesture on the frame
+            current_gesture = GESTURE_REPOSITORY.current_gesture
+            LOGGER.debug("Current gesture: %s", current_gesture)
+            cv.putText(video_frame.image, str(current_gesture), (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            cv.imshow('Video', video_frame.image)
+
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
