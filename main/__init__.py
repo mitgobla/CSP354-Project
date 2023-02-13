26c10cacf979e6ebe01bf01234eaa499a08dd53f
Ben Dodd <ben-dodd@outlook.com>
2023-02-13 21:53:23 +0000

    Update logger format

--- main/__init__.py
+++ main/__init__.py
@@ -1,5 +1,7 @@
 import logging
 import sys
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG,
+    format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+    datefmt="%d/%m/%Y %H:%M:%S")
 LOGGER = logging.getLogger(__name__)
