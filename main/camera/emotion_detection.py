b4980a4d360f000c623f8e967979ef1cea5e895d
Ben Dodd <ben-dodd@outlook.com>
2023-02-18 16:36:35 +0000

    Use WorkerThread

--- main/camera/emotion_detection.py
+++ main/camera/emotion_detection.py
@@ -13,20 +13,63 @@ from . import LOGGER
 from .emotion_repository import EMOTION_REPOSITORY
 from .video_feed import VideoFrame
 
-class EmotionDetection(threading.Thread):
-    """Thread for detecting emotions in a frame
-    """
-    def __init__(self, frame: VideoFrame):
-        super().__init__()
-        self.__image = frame.image
+from ..threading.worker_manager import WORKER_MANAGER
+from ..threading.worker_thread import WorkerThread
+from ..util.singleton import Singleton
 
-    def run(self):
-        try:
-            face_analysis = DeepFace.analyze(self.__image, actions = ['emotion'], enforce_detection=False, silent = True)[0]
-            dominant_emotion = face_analysis['dominant_emotion']
-            EMOTION_REPOSITORY.current_emotion = dominant_emotion
-        except cv.error:
-            return
+class EmotionDetection(metaclass=Singleton):
+    """
+    Class for detecting emotions in a video frame.
+    """
+
+    class EmotionDetectionWorker(WorkerThread):
+        """
+        Worker thread for the emotion detection.
+        """
+        running = False
+
+        def __init__(self, emotion_detection: "EmotionDetection"):
+            """Create a new instance of the EmotionDetectionWorker class."""
+            super().__init__()
+            self.emotion_detection = emotion_detection
+
+        def work(self):
+            """
+            Run the worker thread.
+            """
+            if self.running or self.emotion_detection.image is None:
+                return
+            try:
+                face_analysis = DeepFace.analyze(self.emotion_detection.image, actions = ['emotion'], enforce_detection=False, silent = True)[0]
+                dominant_emotion = face_analysis['dominant_emotion']
+                EMOTION_REPOSITORY.current_emotion = dominant_emotion
+            except cv.error:
+                return
+
+    def __init__(self):
+        super().__init__()
+        self.__lock = threading.Lock()
+        self.__image = None
+
+    @property
+    def image(self):
+        """
+        Get the image to analyse.
+        """
+        with self.__lock:
+            return self.__image
+
+    @image.setter
+    def image(self, frame: VideoFrame):
+        """
+        Set the image to analyse.
+        """
+        with self.__lock:
+            self.__image = frame.image
+            worker = self.EmotionDetectionWorker(self)
+            WORKER_MANAGER.add_thread(worker)
+
+EMOTION_DETECTION = EmotionDetection()
 
 if __name__ == "__main__":
     from .video_feed import VIDEO_FEED
@@ -34,12 +77,11 @@ if __name__ == "__main__":
     while True:
         video_frame = VIDEO_FEED.capture()
         if video_frame:
-            emotion_detection = EmotionDetection(video_frame)
-            emotion_detection.start()
-            emotion_detection.join()
+            EMOTION_DETECTION.image = video_frame
             # draw the emotion on the frame
             current_emotion = EMOTION_REPOSITORY.current_emotion
             cv.putText(video_frame.image, EMOTION_REPOSITORY.current_emotion, (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             cv.imshow('Video', video_frame.image)
         if cv.waitKey(1) & 0xFF == ord('q'):
+            WORKER_MANAGER.stop_threads()
             break
