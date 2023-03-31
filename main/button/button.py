6f75858d7b95c33e8fe1ab5c4098a059f5047b4e
Ben Dodd <ben-dodd@outlook.com>
2023-03-31 20:48:45 +0100

    Simplification of Button

--- main/button/button.py
+++ main/button/button.py
@@ -3,8 +3,9 @@ Driver class for a button.
 Author: Benjamin Dodd (1901386)
 """
 
+from threading import Lock
+
 from . import LOGGER
-from .button_repository import ButtonRepository
 
 try:
     import RPi.GPIO as GPIO
@@ -16,17 +17,21 @@ class Button:
     Driver class for a button.
     """
 
-    def __init__(self, pin: int):
+    def __init__(self, name: str, pin: int):
         """Create a new instance of the Button class."""
+
+        self.name = name
         self.pin = pin
-        self.button_repository = ButtonRepository()
+
+        self._lock = Lock()
+        self._state = False
         self._setup()
 
     def __repr__(self):
-        return f"Button({self.pin})"
+        return f"Button({self.name}, {self.pin})"
 
     def __str__(self):
-        return f"Button({self.pin})"
+        return self.__repr__()
 
     def _setup(self):
         """
@@ -35,12 +40,30 @@ class Button:
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-        GPIO.add_event_detect(self.pin, GPIO.BOTH, callback = self._update_repository, bouncetime = 200)
+        GPIO.add_event_detect(self.pin, GPIO.BOTH, callback = self._update_state, bouncetime = 200)
 
-        if not self.button_repository.has_button(self):
-            self.button_repository.update_button_state(self, False)
+    def _update_state(self):
+        with self._lock:
+            self._state = not GPIO.input(self.pin)
 
-    def _update_repository(self):
-        LOGGER.debug("Updating button state for %s", self)
-        self.button_repository.update_button_state(self, not GPIO.input(self.pin))
+    @property
+    def state(self):
+        """
+        Get the current state of the button.
+        """
+        with self._lock:
+            return self._state
 
+    def wait_for_press(self):
+        """
+        Wait for the button to be pressed.
+        """
+        while not self.state:
+            pass
+
+    def wait_for_release(self):
+        """
+        Wait for the button to be released.
+        """
+        while self.state:
+            pass
