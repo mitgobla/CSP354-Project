15c1ff16587fc922e874fa01b2d633650ad2d2fa
Ben Dodd <ben-dodd@outlook.com>
2023-04-03 11:25:30 +0100

    Activity Selector

--- main/run.py
+++ main/run.py
@@ -15,6 +15,7 @@ from .camera.emotion_detection import EmotionDetection
 from .activities.clock import ClockActivity
 from .activities.emotion_reaction import EmotionReactionActivity
 from .activities.number_guessing import NumberGuessingActivity
+from .activities.activity_selector import ActivitySelector
 
 from .threading.worker_manager import WorkerManager
 
@@ -33,3 +34,18 @@ EMOTION_DETECTION = EmotionDetection(WORKER_MANAGER)
 CLOCK_ACTIVITY = ClockActivity(WORKER_MANAGER, LEFT_DISPLAY, RIGHT_DISPLAY, BUTTON)
 EMOTION_REACTION_ACTIVITY = EmotionReactionActivity(WORKER_MANAGER, LEFT_DISPLAY, RIGHT_DISPLAY, EMOTION_DETECTION, VIDEO_FEED)
 NUMBER_GUESSING_ACTIVITY = NumberGuessingActivity(WORKER_MANAGER, LEFT_DISPLAY, RIGHT_DISPLAY, VIDEO_FEED, GESTURE_DETECTION)
+
+ACTIVITIES = [CLOCK_ACTIVITY, EMOTION_REACTION_ACTIVITY, NUMBER_GUESSING_ACTIVITY]
+ACTIVITY_SELECTOR = ActivitySelector(WORKER_MANAGER, ACTIVITIES, LEFT_DISPLAY, RIGHT_DISPLAY, BUTTON)
+
+def main():
+    """
+    Main function to run the application
+    """
+    LOGGER.info("Starting application")
+    ACTIVITY_SELECTOR.start()
+    LOGGER.info("Application started")
+    ACTIVITY_SELECTOR.join()
+
+if __name__ == "__main__":
+    main()
