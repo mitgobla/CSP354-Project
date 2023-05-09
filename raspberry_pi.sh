9999a2e6d44ef07a7ff7225e0870d273789e2b4e
Ben Dodd <ben-dodd@outlook.com>
2023-05-09 01:00:27 +0100

    Raspberry PI Script

---
+++ raspberry_pi.sh
@@ -0,0 +1,10 @@
+# Run script on startup inside /etc/rc.local before exit 0
+# Ensure it is executable: chmod +x /etc/rc.local
+# Include ampersand at end of command to run in background
+
+# Update git repo
+git pull
+
+# Run python script
+
+python3.7 -m main.run
